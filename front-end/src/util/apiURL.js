04d0f5d655dccc24c8703df96b40c86ce26d1c89
Jordan Bobadilla <jordanboba08@gmail.com>
2021-12-01 21:37:58 -0500

    update apiURL with new backend link

--- front-end/src/util/apiURL.js
+++ front-end/src/util/apiURL.js
@@ -1,5 +1,5 @@
 export const apiURL = () => {
   return window.location.hostname === "localhost"
     ? "http://localhost:3333"
-    : "https://dry-meadow-09210.herokuapp.com";
+    : "https://givn.herokuapp.com/";
 };
